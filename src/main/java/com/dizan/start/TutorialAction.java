87049336aa3b7ca3632df2d8c9017319747420b9
lionel <8836272@qq.com>
2012-12-08 22:25:19 +0800

    add the service and how to print value in action

--- src/main/java/com/dizan/start/TutorialAction.java
+++ src/main/java/com/dizan/start/TutorialAction.java
@@ -1,9 +1,23 @@
 package com.dizan.start;
 
+import com.dizan.start.service.FinderService;
+import com.dizan.start.service.FinderServiceInterface;
+
 public class TutorialAction {
+    private String bestService;
+    
+	public String getBestService() {
+		return bestService;
+	}
+
+	public void setBestService(String bestService) {
+		this.bestService = bestService;
+	}
 
 	public String execute(){
-		System.out.println("hello from Execute! ");
+		FinderServiceInterface service = new FinderService();
+		String bestService = service.getPassTutorialService();
+		setBestService(bestService);
 		return "success";
 	}
 	
--- src/main/webapp/success.jsp
+++ src/main/webapp/success.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
-
+<%@ taglib prefix="s" uri="/struts-tags"%>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
   <head>
@@ -19,5 +19,6 @@
   
   <body>
     Success <br>
+    <s:property value="bestService"/>
   </body>
 </html>
